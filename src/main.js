6a4a6e30d95de391fe9834ac2a1ff18d6f024a70
boungkwan <casiru.dev@gmail.com>
2017-10-14 23:08:33 +0900

    Fix exporting bug in main

--- src/main.js
+++ src/main.js
@@ -1,18 +1,4 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './Pagination.vue'
+import Pagination from './Pagination.vue'
 
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App v-on:update="onChange" :maxPage="10" tabletLocation="left"/>',
-  methods: {
-    onChange (page) {
-      console.log(page)
-    }
-  },
-  components: { App }
-})
+export default Pagination
+export { Pagination }
